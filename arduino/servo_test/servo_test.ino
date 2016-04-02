b5fc4086fbbe8a058972aab76806bd6e773691c8
fjctp <snchan20@yahoo.com>
2016-04-02 00:07:39 -0700

    update arduino/servo_test to accept speed command over serial port

--- arduino/servo_test/servo_test.ino
+++ arduino/servo_test/servo_test.ino
@@ -1,31 +1,43 @@
 #include <Servo.h>
 
+#define PIN 2
+#define PULSEMIN 1
+#define PULSEMAX 1
+
 Servo s1;
-int PWMPIN = 2;
-bool toggle = true;
+char buffer[128];
+int8_t i = 0;
+uint8_t speed = 0;
+bool ready = false;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
-  s1.attach(PWMPIN, 1, 2);
-  s1.write(0);
+  s1.attach(PIN, PULSEMIN, PULSEMAX);
+  s1.write(90);
   
-  Serial.println("start");
-  
+  Serial.println("Enter a number between 0 and 180");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  Serial.println("live");
-  if (toggle)
-  {
-    s1.write(90);
-    toggle = false;
+  if(Serial.available()>0) {
+    buffer[i] = Serial.read();
+    if(buffer[i]==';') {
+      buffer[i] = '\0'; // replace ';' with '\0'
+      speed = atoi(buffer); // convert string to int
+
+      // reset
+      ready = true;
+      i = -1;
+    }
+    i++;
   }
-  else
-  {
-    s1.write(0);
-    toggle = true;
+
+  if(ready) {
+    Serial.print("Sending: ");
+    Serial.println(speed, DEC);
+    s1.write(speed);
+    ready = false;
   }
-  delay(3 * 1000);
 }
